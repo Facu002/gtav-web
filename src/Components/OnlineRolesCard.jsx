a7363a9b26cc8a1a9d676c273dbf43bd960815c7
Facu002 <achafacundoagustin@gmail.com>
2023-01-18 16:50:50 -0300

    commit 0.9 (remove framer-motion)

--- src/Components/OnlineRolesCard.jsx
+++ src/Components/OnlineRolesCard.jsx
@@ -1,5 +1,5 @@
 import {React, useState} from 'react'
-import { motion, AnimatePresence } from "framer-motion";
+// import { motion, AnimatePresence } from "framer-motion";
 
 function OnlineRolesCard(props) {
     const [toggle, setToggle] = useState(false)
@@ -25,39 +25,39 @@ const item = {
             toggle
             && (
                 
-            <AnimatePresence  variants={item} initial="visible" animate="hidden">
+            // <AnimatePresence  variants={item} initial="visible" animate="hidden">
                 
-            <motion.div className='modal'>
-                <motion.div className="modal-content"
-                initial={{ opacity: 1,  scale: 0.9}}
-                animate={{
-                opacity: 1,
-                // x: [0, -50, 0],
-                scale: 1,
-                transition: { type: "spring", stiffness: 100, duration: 1}}}
-                exit={{ opacity: 0}}
-                viewport={{ once: false }}
+            <div className='modal'>
+                <div className="modal-content"
+                // initial={{ opacity: 1,  scale: 0.9}}
+                // animate={{
+                // opacity: 1,
+                // // x: [0, -50, 0],
+                // scale: 1,
+                // transition: { type: "spring", stiffness: 100, duration: 1}}}
+                // exit={{ opacity: 0}}
+                // viewport={{ once: false }}
                 >
-                    <motion.div  className="left" 
-                    initial={{ opacity: 1, scale:0.5}}
-                    animate={{
-                    opacity: 1,
-                    y: 0,
-                    scale: 1,
-                    transition: {duration: 0.2,stiffness: 1}
-                    }}
+                    <div  className="left" 
+                    // initial={{ opacity: 1, scale:0.5}}
+                    // animate={{
+                    // opacity: 1,
+                    // y: 0,
+                    // scale: 1,
+                    // transition: {duration: 0.2,stiffness: 1}
+                    // }}
                     // exit={{ opacity: 0}}   
                     >
                         <img src={props.promo} className='promo'/>
                         <div className="container-promo" style={{backgroundImage:`${props.bg}`}}>
                         </div>
-                    </motion.div>
+                    </div>
 
-                    <motion.div className="right" initial="hidden" animate="visible" variants={item}> 
+                    <div className="right" initial="hidden" animate="visible" variants={item}> 
                         
                         <button className='close-btn' onClick={()=>setToggle(!toggle)}>X</button>
 
-                        <motion.div className="container-text"
+                        <div className="container-text"
                         variants={item}
                         >
                         <div>
@@ -77,13 +77,13 @@ const item = {
                             </div>
                         </div>
                         
-                        </motion.div>
-                    </motion.div>
+                        </div>
+                    </div>
 
-                </motion.div>
-            </motion.div>
+                </div>
+            </div>
 
-            </AnimatePresence>
+            // </AnimatePresence>
             ) 
         }
         </>
--- src/Components/OnlineStrikeCard.jsx
+++ src/Components/OnlineStrikeCard.jsx
@@ -1,14 +1,14 @@
 import {React, useState, useEffect} from 'react'
-import { motion, AnimatePresence } from "framer-motion";
+// import { motion, AnimatePresence } from "framer-motion";
 
 
 function OnlineStrikeCard(props) {
     const [toggle, setToggle] = useState(false)
     
-    const item = {
-        visible: { opacity: [0,0,1], x: 0, transition: {duration: 0.3,stiffness: 1}},
-        hidden: { opacity: [0,0,0], x: [-500, -500, 0], transition: {duration: 1,stiffness: 1}},
-    }
+    // const item = {
+    //     visible: { opacity: [0,0,1], x: 0, transition: {duration: 0.3,stiffness: 1}},
+    //     hidden: { opacity: [0,0,0], x: [-500, -500, 0], transition: {duration: 1,stiffness: 1}},
+    // }
     return (
         <>
         <div className='StrikeCard-container' onClick={()=>setToggle(!toggle)}>
@@ -25,39 +25,41 @@ function OnlineStrikeCard(props) {
         {
             toggle
             &&(
-            <AnimatePresence  variants={item} initial="visible" animate="hidden">
+            // <AnimatePresence  variants={item} initial="visible" animate="hidden">
                 
-            <motion.div className='modal'>
-                <motion.div className="modal-content"
-                initial={{ opacity: 1,  scale: 0.9}}
-                animate={{
-                opacity: 1,
-                // x: [0, -50, 0],
-                scale: 1,
-                transition: { type: "spring", stiffness: 100, duration: 1}}}
-                exit={{ opacity: 0}}
-                viewport={{ once: false }}
+            <div className='modal'>
+                <div className="modal-content"
+                // initial={{ opacity: 1,  scale: 0.9}}
+                // animate={{
+                // opacity: 1,
+                // // x: [0, -50, 0],
+                // scale: 1,
+                // transition: { type: "spring", stiffness: 100, duration: 1}}}
+                // exit={{ opacity: 0}}
+                // viewport={{ once: false }}
                 >
-                    <motion.div  className="left" 
-                    initial={{ opacity: 1, scale:0.5}}
-                    animate={{
-                    opacity: 1,
-                    y: 0,
-                    scale: 1,
-                    transition: {duration: 0.2,stiffness: 1}
-                    }}
+                    <div  className="left" 
+                    // initial={{ opacity: 1, scale:0.5}}
+                    // animate={{
+                    // opacity: 1,
+                    // y: 0,
+                    // scale: 1,
+                    // transition: {duration: 0.2,stiffness: 1}
+                    // }}
                     // exit={{ opacity: 0}}   
                     >
                         <div className="container-promo" style={{backgroundImage:`${props.bg}`}}>
                         </div>
-                    </motion.div>
+                    </div>
 
-                    <motion.div className="right" initial="hidden" animate="visible" variants={item}> 
+                    <div className="right" 
+                    // initial="hidden" animate="visible" variants={item}
+                    > 
                         
                         <button className='close-btn' onClick={()=>setToggle(!toggle)}>X</button>
 
-                        <motion.div className="container-text"
-                        variants={item}
+                        <div className="container-text"
+                        // variants={item}
                         // initial={{ opacity: 0}}
                         // animate={{
                         // opacity: 1,
@@ -73,13 +75,12 @@ function OnlineStrikeCard(props) {
                             
                         </div>
                         
-                        </motion.div>
-                    </motion.div>
+                        </div>
+                    </div>
 
-                </motion.div>
-            </motion.div>
+                </div>
+            </div>
 
-            </AnimatePresence>
             )
         }
         </>
